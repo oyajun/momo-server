656546b7c8ba15d84f440bbdf634980caf86af95
oyajun <oyamadajun@outlook.jp>
2025-07-05 22:13:38 +0900

    record get zod

--- app/api/v1/record/route.ts
+++ app/api/v1/record/route.ts
@@ -1,8 +1,32 @@
 import { headers } from "next/headers";
+import * as z from "zod/v4";
 import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 
-type RecordType = "PUBLISHED_BOOK" | "ORIGINAL_BOOK" | "NO_BOOK";
+const baseSchema = z.object({
+  amount: z
+    .number()
+    .int()
+    .min(1) //1h
+    .max(60 * 24), //24h
+  comment: z.optional(z.string()),
+  dateUTC: z.iso.datetime(),
+  dateLocal: z.iso.datetime(),
+});
+
+const PublishedBook = baseSchema.extend({
+  type: z.literal("PUBLISHED_BOOK"),
+  isbn: z.string().length(13),
+});
+
+const OriginalBook = baseSchema.extend({
+  type: z.literal("ORIGINAL_BOOK"),
+  originalBookId: z.bigint(),
+});
+
+const NoBook = baseSchema.extend({
+  type: z.literal("NO_BOOK"),
+});
 
 export async function POST(request: Request) {
   const session = await auth.api.getSession({
@@ -13,67 +37,24 @@ export async function POST(request: Request) {
     return new Response("Unauthorized", { status: 401 });
   }
 
-  const body = await request.json();
-  const {
-    type,
-    amount,
-    comment,
-    dateUTC,
-    timeDifference,
-    isbn,
-    originalBookId,
-  } = body;
+  const body = await request.json().catch((_error) => {
+    return new Response("Invalid JSON", { status: 400 });
+  });
 
-  if (!type || typeof amount !== "number" || !dateUTC || !timeDifference) {
+  const parsedBody = z
+    .discriminatedUnion("type", [PublishedBook, OriginalBook, NoBook])
+    .safeParse(body);
+  if (!parsedBody.success) {
+    console.error("Zod validation error:", parsedBody.error);
     return new Response("Invalid request body", { status: 400 });
-  }
-
-  // 共通フィールド
-  const baseData: {
-    userId: string;
-    type: RecordType;
-    amount: number;
-    comment?: string;
-    dateUTC: Date;
-    timeDifference: number;
-
-    isbn?: string;
-    originalBookId?: bigint;
-  } = {
-    userId: session.user.id,
-    type,
-    amount,
-    comment,
-    dateUTC: new Date(dateUTC),
-    timeDifference,
-  };
-
-  if (type === "PUBLISHED_BOOK") {
-    if (!isbn) {
-      return new Response("Missing isbn for published book", { status: 400 });
-    }
-    baseData.isbn = isbn;
-  } else if (type === "ORIGINAL_BOOK") {
-    if (!originalBookId) {
-      return new Response("Missing originalBookId for original book", {
-        status: 400,
-      });
-    }
-    try {
-      baseData.originalBookId = BigInt(originalBookId);
-    } catch (_error) {
-      return new Response("originalBookId is not BigInt", {
-        status: 400,
-      });
-    }
-  } else if (type === "NO_BOOK") {
-  } else {
-    return new Response("Invalid type", { status: 400 });
   }
 
   try {
     await prisma.record.create({
-      data: baseData,
+      data: {
+        ...parsedBody.data,
+        userId: session.user.id,
+      },
     });
 
     return new Response(null, {
